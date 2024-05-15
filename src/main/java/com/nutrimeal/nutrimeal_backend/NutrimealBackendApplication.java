0200798087695207761c2d8d9e76f0e37b13c187
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-15 22:38:51 +0700

    init project

---
+++ src/main/java/com/nutrimeal/nutrimeal_backend/NutrimealBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrimeal.nutrimeal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimealBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrimealBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutrimeal/nutrimeal_backend/NutrimealBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrimeal.nutrimeal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimealBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
